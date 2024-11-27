69e62145dda06d3df3c583296e9dab1ccf05b63c
pinguping <21024120@myrp.edu.sg>
2024-11-27 15:56:24 +0800

    donez

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,9 +5,25 @@ export default function Form({ onAddTask }) {
 
   const handleSubmit = (e) => {
     // TODO: write code to handle form submission
+    e.preventDefault();
+    if (description.trim()) {
+      const newTask = {id: Date.now(), description, completed: false, };
+      onAddTask(newTask);
+      setDescription(""); // <-- clear input
+    }
+    
   };
 
-  return {
+  return (
     /*TODO: add a form to add a new task*/
-  };
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Enter task"
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
 }
